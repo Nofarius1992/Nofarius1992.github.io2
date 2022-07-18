7b71334a081dcfe497bb1e13c7bb00c2e48f15bc
MacOS <macos@MacBook-Pro-MacOS.local>
2022-07-18 11:02:01 +0300

    Completed lesson 88. Finished the layout of the desktop version of the site.

--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,10 @@ closeElem.addEventListener('click', () => {
 menuOverlay.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const percents = document.querySelectorAll(".skill__percent"),
+      lines   = document.querySelectorAll(".skill__top-line");
+
+percents.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
